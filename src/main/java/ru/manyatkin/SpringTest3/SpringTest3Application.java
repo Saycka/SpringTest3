bcca2ecf508388103861bba0110d1704a0bb7dc6
Andrey <a>
2018-12-30 13:10:46 +0300

    commit1

---
+++ src/main/java/ru/manyatkin/SpringTest3/SpringTest3Application.java
@@ -0,0 +1,14 @@
+package ru.manyatkin.SpringTest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTest3Application.class, args);
+	}
+
+}
+
